15aa4907428a4e73f45571e96e3180de3e63ee60
OjekPantai <elvien.purnawan01@gmail.com>
2025-12-01 18:48:33 +0700

    remove type auth.ts

--- types/auth.ts
+++ types/auth.ts
@@ -1,17 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-export interface LoginFormData {
-  email: string;
-  password: string;
-}
-export interface LoginFormProps extends Omit<React.ComponentProps<'div'>, 'onError'> {
-  onLoginError?: (data: string) => void;
-  onError?: (error: string) => void;
-  redirectTo?: string;
-}
+// export interface LoginCredentials {
+//   email: string;
+//   password: string;
+// }
 
-export interface FormErrors {
-  email?: string;
-  password?: string;
-  general?: string;
-}
+// export interface LoginFormProps extends Omit<React.ComponentProps<'div'>, 'onError'> {
+//   onLoginError?: (data: string) => void;
+//   onError?: (error: string) => void;
+//   redirectTo?: string;
+// }
+
+// export interface FormErrors {
+//   email?: string;
+//   password?: string;
+//   general?: string;
+// }
