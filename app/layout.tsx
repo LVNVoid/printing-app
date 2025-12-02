89a26b6197aec82930a91fd4581df3b9d934acd6
OjekPantai <elvien.purnawan01@gmail.com>
2025-12-02 10:05:14 +0700

    feat: Add root layout with top loader providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { SessionProvider } from './providers/session-provider';
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} antialiased font-sans`}
       >
+        <NextTopLoader />
         <SessionProvider>
           <ThemeProvider
             attribute="class"
